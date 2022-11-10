21e8584ee08209a6d2a4246abd05659ed6f375f2
stelinmariacj <72865736+stelinmariacj@users.noreply.github.com>
2022-11-10 11:26:08 +0530

    weatherapp completed

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "66f0285280mshba36d5fd06ce77fp1c0327jsnd33b21560cd5",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "87435c061140582a482d12c1b38c90cb"; 
